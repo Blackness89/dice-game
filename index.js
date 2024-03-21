847df54a9e7a57fbe12158097388e1629be05a9d
f.adam <144712140+Blackness89@users.noreply.github.com>
2024-03-21 18:16:25 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 var randomNumber1 = Math.floor((Math.random() * 6) + 1);
 var randomDiceImage = "dice" + randomNumber1 + ".png"
 var randomImageSource = "images/" + randomDiceImage;
@@ -9,13 +10,18 @@ var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
+
+
 if (randomNumber1 > randomNumber2 ) {
     document.querySelector("h1").innerHTML = "Player 1 Wins!";
+    document.querySelector("h1").style.color = "coral"
+    
 } 
 else if (randomNumber2 > randomNumber1) {
     document.querySelector("h1").innerHTML = "Player 2 Wins!";
-    document.getElementsByClassName("player1").style.color = blue;
+    document.querySelector("h1").style.color = "#F238F7"
 }
 else {
     document.querySelector("h1").innerHTML = "Draw!";
+    document.querySelector("h1").style.color = "#BBCF15"
 }
